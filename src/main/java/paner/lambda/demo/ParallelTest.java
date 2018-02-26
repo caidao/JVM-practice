e9e64ef9319c95b3736903596687ee1852bb323f
yiwen.pan <yiwen.pan@ele.me>
2018-02-26 09:45:20 +0800

    函数式编程的例子

---
+++ src/main/java/paner/lambda/demo/ParallelTest.java
@@ -0,0 +1,44 @@
+package paner.lambda.demo;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * @User: paner
+ * @Date: 18/2/23 下午3:34
+ */
+public class ParallelTest {
+
+    private static final  int N = 100;
+    private final ThreadLocalRandom random = ThreadLocalRandom.current();
+
+    private Set<String> threadSet = new HashSet<>();
+
+    @Test
+    public void parallelDiceRolls(){
+        double fraction =1.0/ N;
+        IntStream.range(0,N).parallel()
+                .mapToObj(a->twoDiceThrows())
+                .collect(Collectors.groupingBy(side->side,
+                        Collectors.summingDouble(n->fraction)))
+        .entrySet().forEach(System.out::println);
+        System.out.println("thread size:"+threadSet.size());
+
+    }
+
+    public int twoDiceThrows(){
+        int firstThrow = random.nextInt(1, 7);
+        int secondThrow = random.nextInt(1, 7);
+        threadSet.add(Thread.currentThread().getName());
+        return firstThrow + secondThrow;
+    }
+
+}
