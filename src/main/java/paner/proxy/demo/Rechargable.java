5b1feada6eb63c5a8cc4390d624191cb96796512
yiwen.pan <yiwen.pan@ele.me>
2018-02-26 09:52:39 +0800

    update .gitignore

--- src/main/java/paner/proxy/demo/Rechargable.java
+++
@@ -1,8 +0,0 @@
-package paner.proxy.demo;
-
-/**
- * Created by www-data on 17/2/19.
- */
-public interface Rechargable {
-    public void recharge();
-}
--- src/main/java/paner/proxy/demo/Vehicle.java
+++
@@ -1,9 +0,0 @@
-package paner.proxy.demo;
-
-/**
- * Created by www-data on 17/2/19.
- */
-public interface Vehicle {
-
-    public void drive();
-}
